ff21b5e56ac8955b1b4494db547819128c9e0051
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-09 21:30:39 +0530

    Feat(Projects): Add slice to shorten the description of the projects

--- Client/src/components/ui/ProjectCard.tsx
+++ Client/src/components/ui/ProjectCard.tsx
@@ -52,8 +52,11 @@ const ProjectCard = ({ title, description, imgUrl, link, tags, index }: ProjectC
 
         <div className="p-6">
           <h3 className="text-xl font-semibold tracking-tight">{title}</h3>
-          <p className="mt-2 line-clamp-3 text-sm text-muted-foreground">{description}</p>
-          
+          <p className="mt-2 line-clamp-3 text-sm text-muted-foreground">{description.length < 150 ? description : description.slice(0, 150)+"..."} 
+            
+          </p>
+        
+        
           <div className="mt-4 flex flex-wrap gap-2">
             {tags.map((tag, index) => (
               <span 
